0bfa9500d259e1843c92ada34910cbac4509fa24
lawrenceliu2023 <143914799+lawrenceliu2023@users.noreply.github.com>
2023-09-04 20:09:18 -0400

    Add files via upload

---
+++ LawrenceStockAndDashboard.py
@@ -0,0 +1,59 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# Question 1
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
+
+# Question 2
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data=requests.get(url).text
+
+soup = BeautifulSoup(html_data,"html5lib")
+
+tesla_revenue= pd.read_html(url, match="Tesla Quarterly Revenue", flavor='bs4')[0]
+tesla_revenue=tesla_revenue.rename(columns = {'Tesla Quarterly Revenue(Millions of US $)': 'Date', 'Tesla Quarterly Revenue(Millions of US $).1': 'Revenue'})
+tesla_revenue["Revenue"] = tesla_revenue["Revenue"].str.replace(",","").str.replace("$","")
+tesla_revenue.tail()
+
+# Question 3
+gamestop = yf.Ticker("GME")
+gamestop_data = gamestop.history(period="max")
+gamestop_data.reset_index(inplace=True)
+gamestop_data.head()
+
+# Question 4
+url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+html_data=requests.get(url).text
+soup = BeautifulSoup(html_data,"html5lib")
+
+gme_revenue= pd.read_html(url, match="GameStop Quarterly Revenue", flavor='bs4')[0]
+gme_revenue=gme_revenue.rename(columns = {'GameStop Quarterly Revenue(Millions of US $)': 'Date', 'GameStop Quarterly Revenue(Millions of US $).1': 'Revenue'})
+gme_revenue["Revenue"] = gme_revenue["Revenue"].str.replace(",","").str.replace("$","")
+gme_revenue.tail()
+
+# Question 5
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+    
+make_graph(tesla_data, tesla_revenue, 'Tesla Graph')
+
+# Question 6
+make_graph(gamestop_data, gme_revenue, 'GameStop')
